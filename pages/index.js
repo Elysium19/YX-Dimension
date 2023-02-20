75207000c3c586156d54fd694b14d750f159baef
Elysium19 <76473148+Elysium19@users.noreply.github.com>
2023-02-20 10:23:45 +0100

    add last commit

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
 import {useState} from "react";
+import {useRouter} from "next/router";
 
 export default function Home() {
+
+    const router = useRouter()
 
     const [state, setState] = useState(0)
 
@@ -20,7 +23,8 @@ export default function Home() {
                 <div className={styles.nav}>
                     <div>
                         <img className={styles.imgGlobal}
-                             src="https://cdn.discordapp.com/attachments/215603277214121984/1067433991487094844/DALLE_2023-01-24_14.21.17_-_background_purple.png"/>
+                             src="https://cdn.discordapp.com/attachments/215603277214121984/1067433991487094844/DALLE_2023-01-24_14.21.17_-_background_purple.png"
+                             alt={"img"}/>
                     </div>
                     <div>
                         <h1>XY-Dimension</h1>
@@ -31,143 +35,143 @@ export default function Home() {
                 <div className={styles.content}>
                     <div className={styles.warp}>
                         <img className={styles.imgScale}
-                             onClick={() => setState(1)} src="./img1.png"/>
+                             onClick={() => setState(1)} src="./img1.png" alt={"img"}/>
                         {state === 1 &&
                             <img className={state === 1 && styles.imgScaleSelected}
-                                 src="./img1.png"/>
+                                 src="./img1.png" alt={"img"}/>
                         }
 
                         <img className={styles.imgScale}
-                             onClick={() => setState(2)} src="./img1.png"/>
+                             onClick={() => setState(2)} src="./img1.png" alt={"img"}/>
                         {state === 2 &&
                             <img className={styles.imgScaleSelected}
-                                 src="./img1.png"/>
+                                 src="./img1.png" alt={"img"}/>
                         }
 
                         <img className={styles.imgScale}
-                             onClick={() => setState(3)} src="./img1.png"/>
+                             onClick={() => setState(3)} src="./img1.png" alt={"img"}/>
                         {state === 3 &&
                             <img className={styles.imgScaleSelected}
-                                 src="./img1.png"/>
+                                 src="./img1.png" alt={"img"}/>
                         }
 
                         <img className={styles.imgScale}
-                             onClick={() => setState(4)} src="./img1.png"/>
+                             onClick={() => setState(4)} src="./img1.png" alt={"img"}/>
                         {state === 4 &&
                             <img className={styles.imgScaleSelected}
-                                 src="./img1.png"/>
+                                 src="./img1.png" alt={"img"}/>
                         }
 
                         <img className={styles.imgScale}
-                             onClick={() => setState(5)} src="./img1.png"/>
+                             onClick={() => setState(5)} src="./img1.png" alt={"img"}/>
                         {state === 5 &&
                             <img className={styles.imgScaleSelected}
-                                 src="./img1.png"/>
+                                 src="./img1.png" alt={"img"}/>
                         }
 
                         <img className={styles.imgScale}
-                             onClick={() => setState(6)} src="./img1.png"/>
+                             onClick={() => setState(6)} src="./img1.png" alt={"img"}/>
                         {state === 6 &&
                             <img className={styles.imgScaleSelected}
-                                 src="./img1.png"/>
+                                 src="./img1.png" alt={"img"}/>
                         }
 
                         <img className={styles.imgScale}
-                             onClick={() => setState(7)} src="./img1.png"/>
+                             onClick={() => setState(7)} src="./img1.png" alt={"img"}/>
                         {state === 7 &&
                             <img className={styles.imgScaleSelected}
-                                 src="./img1.png"/>
+                                 src="./img1.png" alt={"img"}/>
                         }
 
                         <img className={styles.imgScale}
-                             onClick={() => setState(8)} src="./img1.png"/>
+                             onClick={() => setState(8)} src="./img1.png" alt={"img"}/>
                         {state === 8 &&
                             <img className={styles.imgScaleSelected}
-                                 src="./img1.png"/>
+                                 src="./img1.png" alt={"img"}/>
                         }
 
                         <img className={styles.imgScale}
-                             onClick={() => setState(9)} src="./img1.png"/>
+                             onClick={() => setState(9)} src="./img1.png" alt={"img"}/>
                         {state === 9 &&
                             <img className={styles.imgScaleSelected}
-                                 src="./img1.png"/>
+                                 src="./img1.png" alt={"img"}/>
                         }
 
                         <img className={styles.imgScale}
-                             onClick={() => setState(10)} src="./img1.png"/>
+                             onClick={() => setState(10)} src="./img1.png" alt={"img"}/>
                         {state === 10 &&
                             <img className={styles.imgScaleSelected}
-                                 src="./img1.png"/>
+                                 src="./img1.png" alt={"img"}/>
                         }
 
                         <img className={styles.imgScale}
-                             onClick={() => setState(11)} src="./img1.png"/>
+                             onClick={() => setState(11)} src="./img1.png" alt={"img"}/>
                         {state === 11 &&
                             <img className={styles.imgScaleSelected}
-                                 src="./img1.png"/>
+                                 src="./img1.png" alt={"img"}/>
                         }
 
                         <img className={styles.imgScale}
-                             onClick={() => setState(12)} src="./img1.png"/>
+                             onClick={() => setState(12)} src="./img1.png" alt={"img"}/>
                         {state === 12 &&
                             <img className={styles.imgScaleSelected}
-                                 src="./img1.png"/>
+                                 src="./img1.png" alt={"img"}/>
                         }
 
                         <img className={styles.imgScale}
-                             onClick={() => setState(13)} src="./img1.png"/>
+                             onClick={() => setState(13)} src="./img1.png" alt={"img"}/>
                         {state === 13 &&
                             <img className={styles.imgScaleSelected}
-                                 src="./img1.png"/>
+                                 src="./img1.png" alt={"img"}/>
                         }
 
                         <img className={styles.imgScale}
-                             onClick={() => setState(14)} src="./img1.png"/>
+                             onClick={() => setState(14)} src="./img1.png" alt={"img"}/>
                         {state === 14 &&
                             <img className={styles.imgScaleSelected}
-                                 src="./img1.png"/>
+                                 src="./img1.png" alt={"img"}/>
                         }
 
                         <img className={styles.imgScale}
-                             onClick={() => setState(15)} src="./img1.png"/>
+                             onClick={() => setState(15)} src="./img1.png" alt={"img"}/>
                         {state === 15 &&
                             <img className={styles.imgScaleSelected}
-                                 src="./img1.png"/>
+                                 src="./img1.png" alt={"img"}/>
                         }
 
                         <img className={styles.imgScale}
-                             onClick={() => setState(16)} src="./img1.png"/>
+                             onClick={() => setState(16)} src="./img1.png" alt={"img"}/>
                         {state === 16 &&
                             <img className={styles.imgScaleSelected}
-                                 src="./img1.png"/>
+                                 src="./img1.png" alt={"img"}/>
                         }
 
                         <img className={styles.imgScale}
-                             onClick={() => setState(17)} src="./img1.png"/>
+                             onClick={() => setState(17)} src="./img1.png" alt={"img"}/>
                         {state === 17 &&
                             <img className={styles.imgScaleSelected}
-                                 src="./img1.png"/>
+                                 src="./img1.png" alt={"img"}/>
                         }
 
                         <img className={styles.imgScale}
-                             onClick={() => setState(18)} src="./img1.png"/>
+                             onClick={() => setState(18)} src="./img1.png" alt={"img"}/>
                         {state === 18 &&
                             <img className={styles.imgScaleSelected}
-                                 src="./img1.png"/>
+                                 src="./img1.png" alt={"img"}/>
                         }
 
                         <img className={styles.imgScale}
-                             onClick={() => setState(19)} src="./img1.png"/>
+                             onClick={() => setState(19)} src="./img1.png" alt={"img"}/>
                         {state === 19 &&
                             <img className={styles.imgScaleSelected}
-                                 src="./img1.png"/>
+                                 src="./img1.png" alt={"img"}/>
                         }
 
                         <img className={styles.imgScale}
-                             onClick={() => setState(20)} src="./img1.png"/>
+                             onClick={() => setState(20)} src="./img1.png" alt={"img"}/>
                         {state === 20 &&
                             <img className={styles.imgScaleSelected}
-                                 src="./img1.png"/>
+                                 src="./img1.png" alt={"img"}/>
                         }
 
                     </div>
@@ -176,5 +180,5 @@ export default function Home() {
             </main>
 
         </>
-    )
+    );
 }
